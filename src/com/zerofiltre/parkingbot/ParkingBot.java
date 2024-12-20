1b1d52880bb8da1eac7336e988eef793c06abba1
EBENO <teudjongboumeug@gmail.com>
2024-12-20 16:39:12 +0400

    documentation sur les boucles, exploration des boucles(boucle for| for amelioree| do | do ... while),ignorer des instructions dans une boucle; sortir de la boucle

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -2,38 +2,76 @@ package com.zerofiltre.parkingbot;
 
 import com.zerofiltre.parkingbot.service.ParkingService;
 
+import java.security.SecureRandom;
+
 public class ParkingBot {
 
 
     static String completion = "des equipes du parking Zerofiltre";
     static ParkingService parkingService = new ParkingService();
+    static String[] customers ={"Ebenezaire","Raissa","Chloe","Krishna","Kim","Serena","Ghislaine","Estelle","Merveille","Chanelle","Ines"};
+    static  int parkingSize = new SecureRandom().nextInt(10);
+
 
     /**
      * ceci est la fonction principale d'un programme JAVA
-     * @param args Represente les donnees passees en parametre lors du lancement du programme
+     * @param args Represente tableaux des donnees passees en parametre lors du lancement du programme
      */
-    public static void main(String[] args){
-      sayHello(args[0]);
-      sayBaye();
+    public static void main(String[] args) {
+//        boucleFor();
+        boucleWhile();
+        sayBaye();
     }
+
+    private static void boucleFor() {
+        for (int i = 0; i < customers.length; i++) {
+            sayHello(customers[i]);
+        }
+        for (String customer:customers){
+             sayHello(customer);
+         }
+    }
+
+
+    private static void boucleWhile() {
+        System.out.println("nous avons " + parkingSize + "place(s) restante(s)");
+        int customerNumber = 0;
+        while (parkingSize > 0) {
+            parkingSize--;
+
+            if("Chloe".equals(customers[customerNumber])){
+                customerNumber++;
+                parkingSize++;
+                continue;
+            }
+
+            if ("Krishna".equals(customers[customerNumber])){
+                break;
+            }
+            sayHello(customers[customerNumber]);
+            customerNumber++;
+            System.out.println();
+        }
+    }
+
 
     /**
      * Permet d'acceuillir et de presenter les services a l'utilisateur potentiel du parking
-     * @param arg Represente le nom de l'utilisateur potentiel
+     * @param customer Represente le nom de l'utilisateur potentiel ou le client a qui on dit Hello
      */
-
-
-    private  static void  sayHello(String arg){
-        String welcomeMessage = "Hello "+ arg + ", bienvenue au parking Zerofiltre " + completion;
+    private  static void  sayHello(String customer){
+        String welcomeMessage = "Hello "+ customer + ", bienvenue au parking Zerofiltre " + completion;
         String services = "Nous offrons les services suivants: Gardinage, laverie...";
         String warning = "Depechez-vous d'entrer car il n'y aura bientot plus de places!";
         String parkService = parkingService.parking;
         String washingService = parkingService.washing;
+        String welcomeSentence = "Hello " + customer;
 
         System.out.println(welcomeMessage);
         System.out.println(services);
         System.out.println(warning.toUpperCase());
         System.out.println("Nous proposons les services suivants: " + parkService + " ET " + washingService);
+        System.out.println(welcomeSentence);
     }
 
     private static void  sayBaye(){
